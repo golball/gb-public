ec0a9bc3531e436ac8efae7fbf8c8bf049b79e52
Fede G <giulifed@gmail.com>
2022-09-02 21:31:09 -0300

    finel release

--- deploy-apidocs/entrypoint.sh
+++ deploy-apidocs/entrypoint.sh
@@ -9,7 +9,13 @@ echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
-aws s3 cp ${FILE} s3://${AWS_S3_BUCKET} \
-            --region ${AWS_REGION} $*
+
+# Global config deployed on project subdir
+aws s3 cp apidoc/conf-admin s3://${AWS_S3_BUCKET}/${API-NAME}/conf-admin --region ${AWS_REGION}
+
+
+# Commons definitions deployed on commons subdirs
+aws s3 cp apidoc/schemas s3://${AWS_S3_BUCKET}/schemas --recursive --region ${AWS_REGION}
+aws s3 cp apidoc/parameters s3://${AWS_S3_BUCKET}/parameters --recursive --region ${AWS_REGION}
 
 rm -rf ~/.aws
