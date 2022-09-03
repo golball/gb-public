ca34f162c8929250b6c29324fc5823c3606012d4
Fede G <giulifed@gmail.com>
2022-09-02 21:32:02 -0300

    Fixing name

--- deploy-apidocs/entrypoint.sh
+++ deploy-apidocs/entrypoint.sh
@@ -11,7 +11,7 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 
 # Global config deployed on project subdir
-aws s3 cp apidoc/conf-admin s3://${AWS_S3_BUCKET}/${API-NAME}/conf-admin --region ${AWS_REGION}
+aws s3 cp apidoc/conf-admin s3://${AWS_S3_BUCKET}/${API_NAME}/conf-admin --region ${AWS_REGION}
 
 
 # Commons definitions deployed on commons subdirs
